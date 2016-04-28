4ab9bb302cda2510dbfe0e110fd8578171f78ff4
Erik Pukinskis <erik@snowedin.net>
2016-04-28 00:05:58 -0700

    add HTML inside, before, after, or in place of a DOM node

---
+++ add-html.js
@@ -0,0 +1,66 @@
+var addHtml = (function() {
+
+  var cachedBody
+
+  function body() {
+    if (!cachedBody) {
+      cachedBody = document.querySelector("body")
+    }
+    return cachedBody
+  }
+
+  function addHtmlToContainer(container, newHtml) {
+    var crucible = document.createElement('div')
+
+    crucible.innerHTML = newHtml
+
+    for(var i=0; crucible.children.length; i++) {
+      container.appendChild(crucible.children[i])
+    }
+
+  }
+
+  function addHtml(html) {
+    addHtmlToContainer(body(), html)
+  }
+
+  addHtml.inside = addHtmlToContainer
+
+
+  addHtml.inPlaceOf =
+    function replaceNodeWithHtml(oldChild, newHtml) {
+
+      var newChild = htmlToNode(newHtml)
+
+      var parent = oldChild.parentNode
+
+      parent.replaceChild(newChild, oldChild)
+    }
+
+  function htmlToNode(html) {
+    var crucible = document.createElement('div')
+    crucible.innerHTML = html
+    return crucible.firstChild
+  }
+
+  addHtml.before =
+    function insertHtmlBefore(sibling, newHtml) {
+      var parent = sibling.parentNode
+      var newNode = htmlToNode(newHtml)
+      parent.insertBefore(newNode, sibling)
+    }
+
+  addHtml.after =
+    function insertHtmlAfter(sibling, newHtml) {
+      var newNode = htmlToNode(newHtml)
+
+      if (sibling.nextSibling) {
+        insertHtmlBefore(sibling.nextSibling)
+      } else {
+        var parent = sibling.parentNode
+        parent.appendChild(newNode)
+      }
+    }
+
+  return addHtml
+})()
